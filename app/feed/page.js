f1c63421a9fa556ed2e4c24529429073f92041c3
babaei-mostafa <mostafa.bsh@gmail.com>
2025-08-31 11:36:25 +0800

    added metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,12 +1,21 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from '@/components/posts'
+import { getPosts } from '@/lib/posts'
+
+export async function generateMetadata() {
+  const posts = await getPosts()
+  const numOfPosts = posts.length
+  return {
+    title: `Browse all our ${numOfPosts} posts.`,
+    description: 'Browse all our posts.',
+  }
+}
 
 export default async function FeedPage() {
-  const posts = await getPosts();
+  const posts = await getPosts()
   return (
     <>
       <h1>All posts by all users</h1>
       <Posts posts={posts} />
     </>
-  );
+  )
 }
